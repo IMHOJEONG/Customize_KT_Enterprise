a738ea882d4fe95a76f3af5b9d5103b0b3440904
unknown <39ghwjd@naver.com>
2022-09-26 16:40:49 +0900

    [Change] : add packages & test animation

--- src/views/HomeDrawer.js
+++ src/views/HomeDrawer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useTransition, animated } from '@react-spring/web'
 // import shuffle from 'lodash.shuffle'
 
@@ -9,27 +9,6 @@ import { useTransition, animated } from '@react-spring/web'
 
 export default function HomeDrawer() {
     
-    // return (
-    //   <div className="App">
-       
-    //     {transitions?.map(
-    //       ({ item, key, props }) =>
-    //         item && (
-    //           <animated.div
-    //             key={key}
-    //             style={{ opacity: props.opacity }}
-    //             className="overlay"
-    //           >
-    //             <animated.div style={{ width: props.width }} className="drawer">
-    //               Hey look it's a side drawer!
-    //             </animated.div>
-    //             <div className="fill" onClick={() => setShow(false)} />
-    //           </animated.div>
-    //         )
-    //     )}
-    //   </div>
-    // );
-
   const [show, set] = useState(false)
   const transitions = useTransition(show, {
     from: { position: "fixed", opacity: 0, width: 0 },
@@ -54,36 +33,5 @@ export default function HomeDrawer() {
         }
     </>
   )
-  
-// const [rows, set] = useState(data)
-// useEffect(() => {
-//   const t = setInterval(() => set(shuffle), 2000)
-//   return () => clearInterval(t)
-// }, [])
-
-// let height = 0
-// const transitions = useTransition(
-//   rows.map(data => ({ ...data, y: (height += data.height) - data.height })),
-//   {
-//     key: (item) => item.name,
-//     from: { height: 0, opacity: 0 },
-//     leave: { height: 0, opacity: 0 },
-//     enter: ({ y, height }) => ({ y, height, opacity: 1 }),
-//     update: ({ y, height }) => ({ y, height }),
-//   }
-// )
-
-// return (
-//   <div className={styles.list} style={{ height }}>
-//     {transitions((style, item, t, index) => (
-//       <animated.div className={styles.card} style={{ zIndex: data.length - index, ...style }}>
-//         <div className={styles.cell}>
-//           <div className={styles.details} style={{ backgroundImage: item.css }} />
-//         </div>
-//       </animated.div>
-//     ))}
-//   </div>
-// )
-
 
 }
