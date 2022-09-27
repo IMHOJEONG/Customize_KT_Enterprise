0e1c8e04b30a85b7361c8395a6fbb02102a49c2b
ayeonee <zn_yeon@naver.com>
2022-09-27 19:01:57 +0900

    [ADD] DetailPage 개발 환경 설정

---
+++ src/views/detailPage/Intro/Intro.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Intro = () => {
+  return (
+    <>
+      <p>이런 고민을 한 적 있는 </p>
+      <p>호텔 오너라면?</p>
+    </>
+  );
+};
---
+++ src/views/detailPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Intro } from "./Intro";
+const Detail = () => {
+  return (
+    <>
+      <Intro />
+    </>
+  );
+};
+
+export default Detail;
