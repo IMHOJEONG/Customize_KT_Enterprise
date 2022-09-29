df54f4a7947ae48e05b11fb3aa91c0bd1cc62b1c
ayeonee <zn_yeon@naver.com>
2022-09-29 17:23:26 +0900

    [ADD] DetailPage Install component

--- src/views/detailPage/index.js
+++ src/views/detailPage/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Intro } from "./intro";
 import { IntroImage } from "./introImage";
 import Configuration from "./configuration/Configuration";
+import { InstallInfo } from "./InstallInfo";
 const Detail = () => {
   return (
     <>
       <Intro />
       <IntroImage />
       <Configuration />
+      <InstallInfo />
     </>
   );
 };
