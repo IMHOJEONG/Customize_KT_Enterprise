dc70963140157fb3f62829bff2711c3181f3546a
unknown <39ghwjd@naver.com>
2022-10-03 16:55:40 +0900

    [Divide & Change]: divide & change css, Components

--- src/components/chatbot/BotChatMessage.js
+++ src/components/chatbot/BotChatMessage.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledComment = styled.div`
+    width: 100px;
+    height: 100px;
+    background: 
+`
+
 
 export function BotChatMessage() {
     
