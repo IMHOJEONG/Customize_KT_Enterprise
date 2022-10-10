3cf444049028d2fd2695d9a887f60d84f83e0282
IMHOJEONG <39ghwjd@naver.com>
2022-10-10 18:43:31 +0900

    Updates

---
+++ precache-manifest.9c3e1c9a8b8f44fe64a1332ff025a70b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db48c7a5a8dcafc74373876b4170c3eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a19ec4ba3ad7d06a932",
+    "url": "/static/css/main.76a06cde.chunk.css"
+  },
+  {
+    "revision": "c1d7c76a9838ad74a387",
+    "url": "/static/js/2.ccc16a6f.chunk.js"
+  },
+  {
+    "revision": "b94e771f2e27d6fe9ceb0893e7b3c48c",
+    "url": "/static/js/2.ccc16a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a19ec4ba3ad7d06a932",
+    "url": "/static/js/main.f09c2b45.chunk.js"
+  },
+  {
+    "revision": "2ec310154096ce899172",
+    "url": "/static/js/runtime-main.6ee3cb3e.js"
+  },
+  {
+    "revision": "daf09bc6e3900aeb6f8f092551a9670e",
+    "url": "/static/media/1.daf09bc6.webp"
+  },
+  {
+    "revision": "eb9bcc480da6e275729368c2549cf0ed",
+    "url": "/static/media/2.eb9bcc48.webp"
+  },
+  {
+    "revision": "712089d712f4895c628d096cb200b47b",
+    "url": "/static/media/3.712089d7.webp"
+  },
+  {
+    "revision": "8ee1702b14f590d86d53717d955b6652",
+    "url": "/static/media/KT AI 호텔_브로슈어.8ee1702b.pdf"
+  },
+  {
+    "revision": "ea7fd26a50827e3fd4f4677dcd7e69e6",
+    "url": "/static/media/built-charge.ea7fd26a.png"
+  },
+  {
+    "revision": "daa22ffa4ef414bc0cbb9a12b26c4985",
+    "url": "/static/media/configuration.daa22ffa.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "e52c53763b0fbc3f93522246fe36b4a1",
+    "url": "/static/media/distribution.e52c5376.webp"
+  },
+  {
+    "revision": "8715da8d048d6170374dd90d77b559f9",
+    "url": "/static/media/feature-tile-icon-01.8715da8d.svg"
+  },
+  {
+    "revision": "51c043451b8c18cd9f38d273cd9a300c",
+    "url": "/static/media/feature-tile-icon-02.51c04345.svg"
+  },
+  {
+    "revision": "eee79b4e44b24c2e10ed74451cb7be9d",
+    "url": "/static/media/feature-tile-icon-03.eee79b4e.svg"
+  },
+  {
+    "revision": "9829f8cbef54df8b28074e8fd00d4a68",
+    "url": "/static/media/feature-tile-icon-04.9829f8cb.svg"
+  },
+  {
+    "revision": "fb13f4222609c5f4c9560e8bf405d832",
+    "url": "/static/media/feature-tile-icon-05.fb13f422.svg"
+  },
+  {
+    "revision": "a77dfde8353162e12dd4ef76871e0814",
+    "url": "/static/media/feature-tile-icon-06.a77dfde8.svg"
+  },
+  {
+    "revision": "f3ca8134a46388790e4ef93b6c16eddd",
+    "url": "/static/media/finance.f3ca8134.webp"
+  },
+  {
+    "revision": "c1a882f8cf0807f251a88c7a0c38879f",
+    "url": "/static/media/franchise.c1a882f8.webp"
+  },
+  {
+    "revision": "0aae9a45e3d545d6fc914d25b1b9b761",
+    "url": "/static/media/hospital.0aae9a45.webp"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "6dfd5cb35a9dc74d3a114860be06475c",
+    "url": "/static/media/install-new.6dfd5cb3.png"
+  },
+  {
+    "revision": "83b58499c2ea782b3c2b4bbda9f1076f",
+    "url": "/static/media/install-old.83b58499.png"
+  },
+  {
+    "revision": "47c73dec9151e78f70d995123e2fb080",
+    "url": "/static/media/logo.47c73dec.webp"
+  },
+  {
+    "revision": "341658955bfb1e62ee2d069ccc77b4b5",
+    "url": "/static/media/manufacture.34165895.webp"
+  },
+  {
+    "revision": "c82d7f8f0b5e0c88f91b1aa6c9318425",
+    "url": "/static/media/pattern1.c82d7f8f.png"
+  },
+  {
+    "revision": "60fc02778d59b5dd523ed8604350ee15",
+    "url": "/static/media/pattern2.60fc0277.png"
+  },
+  {
+    "revision": "1509534a3be2c93bce0b53fb094911a9",
+    "url": "/static/media/public.1509534a.webp"
+  },
+  {
+    "revision": "d4f824f5e76175cd2d1db5b34d248130",
+    "url": "/static/media/search_icon.d4f824f5.png"
+  },
+  {
+    "revision": "f54ac3f5916ba1bfc66068dc6b26ef3d",
+    "url": "/static/media/service-charge.f54ac3f5.png"
+  }
+]);
